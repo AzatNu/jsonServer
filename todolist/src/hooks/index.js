54b0be4a3b50ac213b2ac149f91f709154b21099
Azat <152495861+AzatNu@users.noreply.github.com>
2024-09-06 16:33:05 +0300

    Delete todolist directory

--- todolist/src/hooks/index.js
+++
@@ -1,9 +0,0 @@
-import { RequestAddTodoModule } from "./requestAddTodo";
-import { RequestDeleteTodoModule } from "./requestDeleteTodo";
-import { RequestUpdateTodoModule } from "./requestUpdateTodo";
-
-export {
-    RequestAddTodoModule,
-    RequestDeleteTodoModule,
-    RequestUpdateTodoModule,
-};
